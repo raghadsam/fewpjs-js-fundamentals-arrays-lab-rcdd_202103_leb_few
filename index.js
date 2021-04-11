e571c0a776d99f6df4d81b5be0879af44f25ad95
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-11 10:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your solution here!
-const append, prepend, removeLast,removeFirst=["Milo", "Otis", "Garfield"];
+const append=["Milo", "Otis", "Garfield"];
+const prepend=["Milo", "Otis", "Garfield"];
+const removeLast=["Milo", "Otis", "Garfield"];
+const removeFirst=["Milo", "Otis", "Garfield"];
